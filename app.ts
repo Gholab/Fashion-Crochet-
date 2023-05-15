d5386f1d81807c1ba9a948d3e2a7519f4069ed4b
Gholab <hajarelgholabzouri@gmail.com>
2023-05-15 17:38:43 +0200

    currentoutfit

--- app.ts
+++ app.ts
@@ -104,8 +104,8 @@ export class App {
 
     if(localStorage.getItem("currentoutfit")){
       this.currentoutfit=JSON.stringify(localStorage.getItem("currentoutfit"));
-      //this.CreateCharacter(this.currentoutfit+".glb",Vector3.Zero());
-      this.CreateCharacter("init.glb",Vector3.Zero());
+      this.CreateCharacter(this.currentoutfit+".glb",Vector3.Zero());
+      //this.CreateCharacter("init.glb",Vector3.Zero());
     }
     else{
       this.currentoutfit = "";
