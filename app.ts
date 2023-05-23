cab226681e6f08beb1839f691d3aadc8d1aa6e0a
Gholab <hajarelgholabzouri@gmail.com>
2023-05-23 10:12:31 +0200

    camera 2.5

--- app.ts
+++ app.ts
@@ -75,9 +75,9 @@ export class App {
     this.camera.lowerRadiusLimit = 2;
     this.camera.upperRadiusLimit = 10;
     this.camera.wheelDeltaPercentage = 0.01;
-    this.camera.lowerBetaLimit = -Math.PI / 4;
-    this.camera.upperBetaLimit = Math.PI / 4;
-    console.log("camera est changee");
+    this.camera.lowerBetaLimit = -Math.PI / 2.5;
+    this.camera.upperBetaLimit = Math.PI / 2.5;
+    console.log("camera est changee 2.5");
     const light = new HemisphericLight("light1", new Vector3(0, 1, 0), this.scene);
     light.intensity = 0.6;
     light.specular = Color3.Black();
